2b725707e9daebbc6f0fdef340a3ddd5c78e39a7
nancyshenouda <Shenouda.nancy@gmail.com>
2021-06-16 21:28:52 -0400

    Driver utils class

---
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -0,0 +1,9 @@
+package com.cybertek.utilities;
+
+public class Driver {
+    /**
+     * creating the private constructor so this class' object is not reachable from outside.
+     */
+    private Driver(){}
+
+}
