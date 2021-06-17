2cdf6819ca73c947cb4823bae1c0e932892704b1
nancyshenouda <Shenouda.nancy@gmail.com>
2021-06-16 21:34:33 -0400

    Driver utils class

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -1,9 +1,22 @@
 package com.cybertek.utilities;
 
+import org.openqa.selenium.WebDriver;
+
 public class Driver {
     /**
      * creating the private constructor so this class' object is not reachable from outside.
      */
     private Driver(){}
+
+    /**
+     * Making our driver instance private so it is not reachable from outside of the class
+     * Also we are making it static because we are going to use it in a static method
+     * Also, we need the functionality of static (runs before every thing else)
+     */
+    private static WebDriver driver;
+
+    public static WebDriver getDriverMethod(){
+
+    }
 
 }
