03be8ed32fa8e93a0c189b8f2e8cefe67ce3913d
nancyshenouda <Shenouda.nancy@gmail.com>
2021-06-19 10:39:46 -0400

    day12 DriverUtilPractice.java

---
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecuter/DriverUtilPractice.java
@@ -0,0 +1,14 @@
+package com.cybertek.tests.day12_actions_upload_jsexecuter;
+
+import org.testng.annotations.Test;
+
+public class DriverUtilPractice {
+
+    @Test
+    public void simple_google_search_test(){
+        //1- go to google.com
+        //2- search for a value
+        //3- verify value is contained in the title
+
+    }
+}
